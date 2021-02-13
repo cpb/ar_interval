74ed05d9e6a7a0ac6c1bf85281f83baa10b2ff94
Caleb Buxton <me@cpb.ca>
2021-02-13 15:50:25 -0800

    style(test): Minitest::Spec

--- test/ar_interval_test.rb
+++ test/ar_interval_test.rb
@@ -1,11 +1,11 @@
 require "test_helper"
 
-class ArIntervalTest < Minitest::Test
-  def test_that_it_has_a_version_number
+describe ArInterval do
+  it "has a version number" do
     refute_nil ::ArInterval::VERSION
   end
 
-  def test_it_does_something_useful
+  it "has Occurence test model configured" do
     assert Occurence.count == 0
   end
 end
